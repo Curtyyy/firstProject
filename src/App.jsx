131e8c7e620f2257838e68880c8858cc51f4e62e
curt <curtemba12@gmail.com>
2023-07-29 22:24:12 +0800

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import './tailwind.css'
 
 const App = () => {
   return (
-    <div className='bg-black text-white outline'>App</div>
+    <div className='bg-black text-white outline'>
+      <div className='bg-black text-white outline'>App</div>
+      <div className='bg-black text-white outline'>App</div>
+      <div className='bg-black text-white outline'>App</div>
+      <div className='bg-black text-white outline'>App</div>
+      <div className='bg-black text-white outline'>App</div>
+      <div className='bg-black text-white outline'>App</div>
+    </div>
   )
 }
 
